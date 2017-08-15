d7b0c3ef50463c8d5bd4880e743ba34726e4c75b
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-08-15 06:57:54 -0700

    set root path for logged in admins

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :scrape_fails, only: [:index, :show, :update]
 
   devise_for :admins
+
+  authenticated :admin do
+    root to:'scrape_fails#index'
+  end
+
   root to: 'welcome#index'
 
 end
