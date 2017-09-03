ed3d1be31388a9271e1ebeccadcefb5c6a0fd2d0
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-09-03 13:29:50 -0700

    Fix Emily’s list scraper

--- lib/scraper/emilys_list.rb
+++ lib/scraper/emilys_list.rb
@@ -92,8 +92,10 @@ module Scraper
 
       date_node = find_start_date_node(title_node)
       schedule_node = page.at('p:contains("Schedule")')
-      event['start_date'] = parse_start_date(date_node, schedule_node)
+      start_date = parse_start_date(date_node, schedule_node)
+      return unless start_date
 
+      event['start_date'] = start_date
       event['free'] = payment_button_present?(page)
 
       if date_lumped_with_location_data?(date_node)
@@ -130,12 +132,12 @@ module Scraper
       else
         schedule = text_without_empty_lines(current_node)
         start_date = schedule[0].text.gsub("\r\n", '')
-        start_time = schedule[1].text.gsub("\r\n", '').split('-')[0]
+        start_time = schedule[1].text.gsub("\r\n", '').split('-')[0] unless schedule[1].nil?
       end
 
-      date_text = start_date + ' ' + start_time
+      date_text = "#{start_date} #{start_time}"
       date_from_node(date_text)
-    rescue TypeError => err
+    rescue TypeError, ArgumentError => err
       # A TypeError: no implicit conversion of nil into String error will be raised when unable to get value for a date_text component
       nil
     end
